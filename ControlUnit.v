3f7e7ff3acc0ef102351d46b9cd02c29c151c6e5
nagihiko0704 <jihae0584@gmail.com>
2018-11-29 14:51:23 +0900

    Complete ControlUnit

--- ControlUnit.v
+++ ControlUnit.v
@@ -21,7 +21,7 @@ module ControlUnit(
 	reg [3:0] Rd = Instr[15:12] ;
 	reg Zbit = Flags;
 	
-	reg [3:0] OpCode = Instr[24:21];
+	reg [3:0] OpCode = Instr[24:21]; 
 	
 	reg PCSrc_R;
 	reg MemtoReg_R;
@@ -40,58 +40,111 @@ module ControlUnit(
 			begin
 				case(Op)
 					2'b00: //ADD, SUB, MOV, CMP
-							begin
-								case(OpCode)
-									4'b0100: //ADD
-										begin
-											PCSrc_R <= 1'b0;
-											MemtoReg_R <= 1'b0;
-											MemWrite_R <= 1'b0;
-											ALUControl_R <= 1'b0;
-											ALUSrc_R <= (Instr[25] == 0 ? 1'b0 : 1'b1);
-											ImmSrc_R <= (Instr[25] == 0 ? 2'b00 : 2'b01);
-											RegWirte_R <= 1'b0;
-											RegSrc_R <= 1'b0;
+						begin
+							case(OpCode)
+								4'b0100: //ADD
+									begin
+										PCSrc_R <= 1'b0;
+										MemtoReg_R <= 1'b0;
+										MemWrite_R <= 1'b0;
+										ALUControl_R <= 1'b0;
+										ALUSrc_R <= (Instr[25] == 0 ? 1'b0 : 1'b1);
+										ImmSrc_R <= (Instr[25] == 0 ? 2'b00 : 2'b01);
+										RegWirte_R <= 1'b0;
+										RegSrc_R <= 1'b0;
+									end
+								4'b0010: //SUB
+									begin
+										PCSrc_R <= 1'b0;
+										MemtoReg_R <= 1'b0;
+										MemWrite_R <= 1'b0;
+										ALUControl_R <= 1'b1;
+										ALUSrc_R <= (Instr[25] == 0 ? 1'b0 : 1'b1);
+										ImmSrc_R <= (Instr[25] == 0 ? 2'b00 : 2'b01);
+										RegWirte_R <= 1'b0;
+										RegSrc_R <= 1'b0;
 										end
-									4'b0010: //SUB
-										begin
-											PCSrc_R <= 1'b0;
-											MemtoReg_R <= 1'b0;
-											MemWrite_R <= 1'b0;
-											ALUControl_R <= 1'b1;
-											ALUSrc_R <= (Instr[25] == 0 ? 1'b0 : 1'b1);
-											ImmSrc_R <= (Instr[25] == 0 ? 2'b00 : 2'b01);
-											RegWirte_R <= 1'b0;
-											RegSrc_R <= 1'b0;
-										end
-									4'b1101: //MOV
-										begin
-											PCSrc_R <= 1'b0;
-											MemtoReg_R <= 1'b0;
-											MemWrite_R <= 1'b0;
-											ALUControl_R <= 1'b0;
-											ALUSrc_R <= (Instr[25] == 0 ? 1'b0 : 1'b1);
-											ImmSrc_R <= (Instr[25] == 0 ? 2'b00 : 2'b01);
-											RegWirte_R <= 1'b0;
-											RegSrc_R <= 1'b0;
-										end
-									4'b1010: //CMP
-										begin
-											PCSrc_R <= 1'b0;
-											MemtoReg_R <= 1'bx;
-											MemWrite_R <= 1'b0;
-											ALUControl_R <= 1'b1;
-											ALUSrc_R <= (Instr[25] == 0 ? 1'b0 : 1'b1);
-											ImmSrc_R <= (Instr[25] == 0 ? 2'b00 : 2'b01);
-											RegWirte_R <= 1'b0;
-											RegSrc_R <= 1'b0;
-										end
-									default:
-								endcase
-							end
-					2'b01: //LDR, STR, B, BL
-							begin
-							end
+								4'b1101: //MOV
+									begin
+										PCSrc_R <= 1'b0;
+										MemtoReg_R <= 1'b0;
+										MemWrite_R <= 1'b0;
+										ALUControl_R <= 1'b0;
+										ALUSrc_R <= (Instr[25] == 0 ? 1'b0 : 1'b1);
+										ImmSrc_R <= (Instr[25] == 0 ? 2'b00 : 2'b01);
+										RegWirte_R <= 1'b0;
+										RegSrc_R <= 1'b0;
+									end
+								4'b1010: //CMP
+									begin
+										PCSrc_R <= 1'b0;
+										MemtoReg_R <= 1'bx;
+										MemWrite_R <= 1'b0;
+										ALUControl_R <= 1'b1;
+										ALUSrc_R <= (Instr[25] == 0 ? 1'b0 : 1'b1);
+										ImmSrc_R <= (Instr[25] == 0 ? 2'b00 : 2'b01);
+										RegWirte_R <= 1'b0;
+										RegSrc_R <= 1'b0;
+									end
+								default:
+							endcase
+						end
+					2'b01: //LDR, STR
+						begin
+							case(Instr[20])
+								1'b0: //STR
+									begin
+										PCSrc_R <= 1'b0;
+										MemtoReg_R <= 1'bx;
+										MemWrite_R <= 1'b1;
+										ALUControl_R <= (Instr[23] == 0 ? 1'b1 : 1'b0);
+										ALUSrc_R <= (Instr[25] == 0 ? 1'b0 : 1'b1);
+										ImmSrc_R <= (Instr[25] == 0 ? 2'b00 : 2'b10);
+										RegWirte_R <= 1'b0;
+										RegSrc_R <= 1'b0;
+									end
+								1'b1: //LDR
+									begin
+										PCSrc_R <= 1'b0;
+										MemtoReg_R <= 1'b1;
+										MemWrite_R <= 1'b0;
+										ALUControl_R <= (Instr[23] == 0 ? 1'b1 : 1'b0);
+										ALUSrc_R <= (Instr[25] == 0 ? 1'b0 : 1'b1);
+										ImmSrc_R <= (Instr[25] == 0 ? 2'b00 : 2'b10);
+										RegWirte_R <= 1'b1;
+										RegSrc_R <= 1'b0;
+									end
+								default: //do nothing
+							endcase
+						end
+					2'b10: //B, BL
+						begin
+							case(Instr[24])
+								1'b0: //B
+									begin
+										PCSrc_R <= 1'b1;
+										MemtoReg_R <= 1'b0;
+										MemWrite_R <= 1'b0;
+										ALUControl_R <= 1'b0;
+										ALUSrc_R <= 1'b1;
+										ImmSrc_R <= 2'b11;
+										RegWirte_R <= 1'b0;
+										RegSrc_R <= 1'b1;
+									end
+								1'b1: //BL
+									begin
+										PCSrc_R <= 1'b1;
+										MemtoReg_R <= 1'b0;
+										MemWrite_R <= 1'b0;
+										ALUControl_R <= 1'b0;
+										ALUSrc_R <= 1'b1;
+										ImmSrc_R <= 2'b11;
+										RegWirte_R <= 1'b0;
+										RegSrc_R <= 1'b1;
+									end
+								default: //do nothing
+							endcase
+						end
 					default: //do nothing
 				endcase
 			end
