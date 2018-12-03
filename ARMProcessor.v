ab3fb3cced56ee42c608a1cfd47c2d668d4f6114
nagihiko0704 <jihae0584@gmail.com>
2018-12-03 22:31:57 +0900

    Save processor

--- ARMProcessor.v
+++ ARMProcessor.v
@@ -14,10 +14,31 @@ module armreduced(
 	assign be = 4'b1111; // default
 	assign memread = 'b1; // default
 	
-	RegisterFile registerfile();
-	ALU alu();
-	ControlUnit controlunit();
-	Extend extend();
+	wire PCSrc;
+	wire MemtoReg;
+	wire MemWrite;
+	wire ALUControl;
+	wire ALUSrc;
+	wire [1:0] ImmSrc;
+	wire RegWrite;
+	wire ALUFlags;
+	wire [31:0] SrcA;
+	wire [31:0] SrcB;
+	wire [31:0] ALUResult;
+	wire [3:0] RA1;
+	wire [3:0] RA2;
+	wire [3:0] RA3;
+	wire [31:0] result;
+	wire [31:0] PCPlus8;
+	wire [31:0] WriteData;
+	wire [31:0] ExtImm;
+	wire [31:0] Instr;
+	
+	
+	RegisterFile registerfile(.RA1(RA1), .RA2(RA2), .RA3(RA3), .WD3(Result), .R15(PCPlus8), .RD1(SrcA), .RD2(), .RD3());
+	ALU alu(.ALUResult(ALUResult), .ALUFlags(ALUFlags), .ScrA(ScrA), .ScrB(SrcB), .ALUControl(ALUControl));
+	ControlUnit controlunit(.Instr(Instr), .Flags(ALURlags), .PCSrc(PCSrc), .MemtoReg(), .ALUControl(), .ALUSrc(), .ImmSrc(), .RegWrite(), .RegSrc());
+	Extend extend(.ExtImm(ExtImm), .Instr(), .ImmSrc(ImmSrc));
 	
 	always@(posedge clk or negedge reset) begin
 		
